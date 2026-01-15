b87b72a24754cd5c0a5130ac40eec16de5e0c024
GitHub Action <action@github.com>
2026-01-15 13:56:51 +0000

    chore: Bump version to 0.4.10 [skip ci]

--- src/ui/tui_app.py
+++ src/ui/tui_app.py
@@ -61,7 +61,7 @@ gaming_opt = GamingOptimizer(optimizer.hw)
 class OptimizerApp:
     """Streamlined Optimization-Only TUI Application"""
     
-    VERSION = "0.4.9"
+    VERSION = "0.4.10"
     
     def __init__(self):
         self.console = Console()
